75b1b3ec80bf7bbd1e368872b695afb13f2ced07
0912gil <anasulma1230@gmail.com>
2024-07-26 23:12:28 -0600

    desafio1

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,68 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
+
+import React, { useEffect, useState } from 'react';
+import Item from './components/Item';
+import crisisData from '../data.json';
+import fraudesData from '../data2.json';
+import './page.module.css';
+
+function App() {
+  const [items, setItems] = useState([]);
+  const [selectedDataTypes, setSelectedDataTypes] = useState({
+    crisis: false,
+    fraudes: false
+  });
+
+  useEffect(() => {
+    let selectedItems = [];
+    if (selectedDataTypes.crisis) {
+      selectedItems = [...selectedItems, ...crisisData.crisis_economicas];
+    }
+    if (selectedDataTypes.fraudes) {
+      selectedItems = [...selectedItems, ...fraudesData.fraudes_cripto];
+    }
+    setItems(selectedItems);
+  }, [selectedDataTypes]);
+
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
+    setSelectedDataTypes((prev) => ({ ...prev, [name]: checked }));
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="App">
+      <div className="checkboxes">
+        <label>
+          <input
+            type="checkbox"
+            name="crisis"
+            checked={selectedDataTypes.crisis}
+            onChange={handleCheckboxChange}
+          />
+          Crisis Económicas
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            name="fraudes"
+            checked={selectedDataTypes.fraudes}
+            onChange={handleCheckboxChange}
+          />
+          Fraudes Cripto
+        </label>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      {items.map((item, index) => (
+        <Item
+          key={index}
+          titulo={item.titulo}
+          descripcion={item.descripcion}
+          caracteristicas={item.caracteristicas}
+          imagen_de_referencia={item.imagen_de_referencia}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      ))}
+    </div>
   );
 }
+
+export default App;
